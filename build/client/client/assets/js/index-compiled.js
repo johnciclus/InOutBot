aa545c9b498633fceced9663f8daaa8bdc77d7fb
johntalent <talent.corp@gmail.com>
2016-12-22 17:52:58 -0500

    Mongo configuration

--- build/client/client/assets/js/index-compiled.js
+++
@@ -1,3 +0,0 @@
-'use strict';/* Card.js plugin by Jesse Pollak. https://github.com/jessepollak/card */var params=location.search.substring(1).split('=');$('<input>').attr({type:'hidden',name:'consumerID',value:params[1]}).appendTo('form');$('form').card({container:'.card-wrapper',width:200,formSelectors:{nameInput:'input[name="holderName"]'}});
-
-//# sourceMappingURL=index-compiled.js.map
